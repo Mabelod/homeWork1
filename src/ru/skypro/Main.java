da8be708f5c779a174e7aa683f6ca7675751bf21
Ivanov Ivan <ivanov93@vk.com>
2022-05-16 19:18:35 +0300

    Задача №5

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,11 +2,14 @@ package ru.skypro;
 
 public class Main {
     public static void main(String[] args){
-        int a = 250;
-        int b = 500;
-        int c = 7000 / a;
-        int n = 7000 / b;
-        System.out.println("При снижении веса 250 грамм в день, спортсмен похудеет через " + c + " дней ");
-        System.out.println("При снижении веса 500 грамм в день, спортсмен похудеет через " + n + " дней ");
+        int maria = 67760;
+        int denis = 83690;
+        int kristina = 76230;
+        int mariaRise = (maria * 10) / 100;
+        int denisRise = (denis * 10) / 100;
+        int kristinaRise = (kristina * 10) / 100;
+        System.out.println("Мария теперь получает " + (maria + mariaRise) + " рублей. Годовой догод вырос на " + mariaRise + " рублей.");
+        System.out.println("Денис теперь получает " + (denis + denisRise) + " рублей. Годовой догод вырос на " + denisRise + " рублей.");
+        System.out.println("Кристина теперь получает " + (kristina + kristinaRise) + " рублей. Годовой догод вырос на " + kristinaRise + " рублей.");
     }
     }
