f16c62e0a055a1e2f992eb90652b2d6a61435e90
Ivanov Ivan <ivanov93@vk.com>
2022-05-16 20:57:46 +0300

    Задача №4 (более подробно расписал переменные, при первом решении не заметил что нужно расчитать среднее кол-во дней для похудения).

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,11 +2,21 @@ package ru.skypro;
 
 public class Main {
     public static void main(String[] args){
-        int a = 250;
-        int b = 500;
-        int c = 7000 / a;
-        int n = 7000 / b;
-        System.out.println("При снижении веса 250 грамм в день, спортсмен похудеет через " + c + " дней ");
-        System.out.println("При снижении веса 500 грамм в день, спортсмен похудеет через " + n + " дней ");
+        task4();
+    }
+    public static void task4(){
+        int weight = 7;
+        int gramsInKg = 1000;
+        int weightInGrams = weight * gramsInKg;
+
+        int min = 250;
+        int max = 500;
+
+        int maxDays = weightInGrams / min;
+        int minDays = weightInGrams / max;
+        int averageDays = (maxDays + minDays) / 2;
+        System.out.println("При снижении веса 250 грамм в день, спортсмен похудеет через " + minDays + " дней ");
+        System.out.println("При снижении веса 500 грамм в день, спортсмен похудеет через " + maxDays + " дней ");
+        System.out.println("В среднем для похудения необходим " + averageDays + " день ");
     }
     }
