6143267ddfcd018dfa5e353cffb0138c0d9a44c8
Ivanov Ivan <ivanov93@vk.com>
2022-05-16 20:27:50 +0300

    Задача №2 (замена if на math.abs)

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,14 +2,14 @@ package ru.skypro;
 
 public class Main {
     public static void main(String[] args){
+        task2();
+    }
+    public static void task2(){
         double boxer1 = 78.2;
         double boxer2 = 82.7;
         double sum = boxer1 + boxer2;
-        double difference = (boxer1 - boxer2) ;
-        if (difference < 0){
-            difference = difference * -1;
-        }
+        double difference = Math.abs(boxer1 - boxer2) ;
         System.out.println("Общий вес боксеров " + sum + " кг");
         System.out.println("Разница в весе " + difference + " кг");
-        }
     }
+}
