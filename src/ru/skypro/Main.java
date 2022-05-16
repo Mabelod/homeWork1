98b80ba077a053c479e85b2f6ec18a19f68d9f01
Ivanov Ivan <ivanov93@vk.com>
2022-05-16 17:35:04 +0300

    Задача №1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,6 +2,12 @@ package ru.skypro;
 
 public class Main {
     public static void main(String[] args){
-
+            byte a = 127;
+            short b = 32767;
+            int c = 158_789;
+            long z = 4567L;
+            float e = 16f;
+            double y = 15.7;
+            System.out.println(a + " " + b + " " + c + " " + z + " " + e + " " + y);
+        }
     }
-}
