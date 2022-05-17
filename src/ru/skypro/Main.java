7db99eac43cbc490b9c30a31baa4847cc38c262b
Ivanov Ivan <ivanov93@vk.com>
2022-05-17 13:18:39 +0300

    Задача повышенной сложности №6

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,14 +5,12 @@ public class Main {
         task5();
     }
     public static void task5(){
-        int maria = 67760;
-        int denis = 83690;
-        int kristina = 76230;
-        int mariaRise = (maria * 10) / 100;
-        int denisRise = (denis * 10) / 100;
-        int kristinaRise = (kristina * 10) / 100;
-        System.out.println("Мария теперь получает " + (maria + mariaRise) + " рублей. Годовой догод вырос на " + (mariaRise * 12) + " рублей.");
-        System.out.println("Денис теперь получает " + (denis + denisRise) + " рублей. Годовой догод вырос на " + (denisRise * 12) + " рублей.");
-        System.out.println("Кристина теперь получает " + (kristina + kristinaRise) + " рублей. Годовой догод вырос на " + (kristinaRise * 12) + " рублей.");
+        byte a = 12;
+        byte b = 27;
+        byte c = 44;
+        byte d = 15;
+        byte e = 9;
+        int result = Math.abs(a * (b + ( c - d * e)));
+        System.out.println(result);
     }
     }
